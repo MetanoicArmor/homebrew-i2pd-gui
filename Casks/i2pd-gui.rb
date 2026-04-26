0ce87be4642e14c94b0d98a7ad4ddd116e0b47b2
Vade <72322348+MetanoicArmor@users.noreply.github.com>
2026-04-26 09:29:56 +0300

    cask: refresh v2.60.0 checksums after latest rebuilt bundles

--- Casks/i2pd-gui.rb
+++ Casks/i2pd-gui.rb
@@ -1,13 +1,13 @@
 cask "i2pd-gui" do
   version "2.60.0"
   on_arm do
-    sha256 "e56a3baa9c333e846ce0ca9e81aaf716fd991e2494ae2c2a9dc5fbac3f82b503"
+    sha256 "63cbe489cbed8cc03ffa10faaf784b824aae6e6a2d51cd8364e08fb573d596bb"
     url "https://github.com/MetanoicArmor/gui-i2pd/releases/download/v#{version}/I2P-Daemon-GUI-#{version}.zip"
     app "I2P Daemon GUI.app"
   end
 
   on_intel do
-    sha256 "a48e42807bf24e2e8b69426ad6ff70da54497591b337d8320781fd048327c56d"
+    sha256 "81727be3fd03139d569b86e3bf4ac8863c09826ad97791e226c77fe1e12b14a1"
     url "https://github.com/MetanoicArmor/gui-i2pd/releases/download/v#{version}/I2P-Daemon-GUI-Intel-#{version}.zip"
     app "I2P Daemon GUI-Intel.app", target: "I2P Daemon GUI.app"
   end
